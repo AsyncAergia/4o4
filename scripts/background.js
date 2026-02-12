7945e4ff9d54e66e251c25d559256d9d5e9a2973
Mukul141 <groth.8r@gmail.com>
2026-02-12 16:05:06 +0530

    Add initial content

---
+++ scripts/background.js
@@ -0,0 +1,14 @@
+const filter = {
+  properties: ["url"],
+};
+
+browser.tabs.onUpdated.addListener(
+  updateTabTo404,
+  filter
+)
+
+function updateTabTo404() {
+    browser.tabs.update(
+        {url: '/assets/404_trial.html'}
+    );
+}
