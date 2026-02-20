0170eaba86b3b23ea83becd500e287ffdec325a5
Mukul141 <groth.8r@gmail.com>
2026-02-20 20:03:07 +0530

    fix: add extension id for firefox

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,8 +5,20 @@ export default defineConfig({
     srcDir: "src",
     modulesDir: "wxt-modules",
     outDir: "dist",
-    manifest: {
+
+    manifest: (env) => ({
     homepage_url: "https://github.com/AsyncAergia/4o4/tree/main",
     permissions: ['tabs'],
-  }
+
+    ...(env.browser === 'firefox' && {
+      browser_specific_settings: {
+        gecko: {
+          id: '4o4@AsyncAergia.io',
+          data_collection_permissions: {
+            required: ['none']
+          }
+        } as any
+      }
+    })
+  })
 });
